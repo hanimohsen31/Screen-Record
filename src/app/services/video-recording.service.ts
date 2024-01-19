7dde6bd26db1f6dd89f9f853d413ba01a1a82647
Hani Rashed <hrashed@getgroup.com>
2024-01-19 19:07:41 +0200

    'npm-update'

--- src/app/services/video-recording.service.ts
+++ src/app/services/video-recording.service.ts
@@ -32,43 +32,52 @@ export class VideoRecordingService {
   }
 
   async handleRecording() {
-    const audioConstraints = {
-      echoCancellation: false,
-      noiseSuppression: false,
-      // sampleRate: 44100, // Adjust this based on your requirements
-      sampleRate: 128000,
-      desiredSampRate: 128000,
-      // bufferSize: 360000,
-      bitrate: 128000,
-      // bitsPerSecond: 128000,
-    };
+    try {
+      const audioConstraints = {
+        echoCancellation: false,
+        noiseSuppression: false,
+        // sampleRate: 44100, // Adjust this based on your requirements
+        sampleRate: 128000,
+        desiredSampRate: 128000,
+        // bufferSize: 360000,
+        bitrate: 128000,
+        // bitsPerSecond: 128000,
+      };
 
-    this.mediaStream = await navigator.mediaDevices.getDisplayMedia({
-      audio: audioConstraints,
-      video: true,
-    });
-
-    this.mediaStream$.next(this.mediaStream);
-    this.recorder = new RecordRTC(this.mediaStream, { type: 'video' });
-
-    let myObject = this.recorder;
-    const originalFunction = myObject.stopRecording;
-    myObject.stopRecording = function () {
-      originalFunction.apply(this, arguments);
-    };
-
-    const videoTrack = this.mediaStream.getVideoTracks()[0];
-
-    videoTrack.addEventListener('ended', () => {
-      this.stopRecording();
-      this.buttonsStatus.next({
-        NONE: false,
-        RECORDING: false,
-        RECORDED: true,
+      this.mediaStream = await navigator.mediaDevices.getDisplayMedia({
+        audio: audioConstraints,
+        video: true,
       });
-    });
 
-    this.recorder.startRecording();
+      this.mediaStream$.next(this.mediaStream);
+      this.recorder = new RecordRTC(this.mediaStream, { type: 'video' });
+
+      let myObject = this.recorder;
+      const originalFunction = myObject.stopRecording;
+      myObject.stopRecording = function () {
+        originalFunction.apply(this, arguments);
+      };
+
+      const videoTrack = this.mediaStream.getVideoTracks()[0];
+
+      videoTrack.addEventListener('ended', () => {
+        this.stopRecording();
+        this.buttonsStatus.next({
+          NONE: false,
+          RECORDING: false,
+          RECORDED: true,
+        });
+      });
+
+      this.recorder.startRecording();
+    } catch (error) {
+      console.error(error);
+      this.buttonsStatus.next({
+        NONE: true,
+        RECORDING: false,
+        RECORDED: false,
+      });
+    }
   }
 
   stopRecording() {
