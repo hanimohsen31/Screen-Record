3dcfe515148989b54c936d5d3d80d6d50f0d7847
Hani Mohsen <hanimohsen3810@yaho.com>
2024-03-12 05:12:10 +0200

    'npm-update'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,18 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { CounterService } from './services/counter.service';
 import { RecorderComponent } from './recorder/recorder.component';
 import { catchError } from 'rxjs';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, RecorderComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent,
+    RecorderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -19,10 +26,12 @@ export class AppComponent {
   constructor(private CounterService: CounterService) {}
 
   ngOnInit() {
-    this.CounterService.handleCounter$.pipe(
-      catchError((error) => {
-        throw new Error('Get Counter Error', error);
-      })
-    ).subscribe()
+    this.CounterService.handleCounter$
+      .pipe(
+        catchError((error) => {
+          throw new Error('Get Counter Error', error);
+        })
+      )
+      // .subscribe();
   }
 }
